d1d3efeb94de6193e38ba68d49c3dc03921de4bf
Arvid <axar1802@student.ju.se>
2022-11-28 12:25:08 +0100

    Initial Commit

---
+++ Yahtzee/YahtzeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YahtzeeApp.swift
+//  Yahtzee
+//
+//  Created by Arvid Axelsson on 2022-11-28.
+//
+
+import SwiftUI
+
+@main
+struct YahtzeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
